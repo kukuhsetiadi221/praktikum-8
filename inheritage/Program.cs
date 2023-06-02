f6d3530ad6517e3cfd28626c7e8330bee98b0a31
kukuhsetiadi221 <kukuuhsetiad@students.amikom.ac.id>
2023-06-02 19:06:13 +0700

    first commit

---
+++ inheritage/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person person1 = new person();
+            person1.Nama = "Adel";
+            person1.Umur = 17;
+            person1.infoperson();
+
+            //inheritage
+            karyawan karyawan1 = new karyawan();
+            karyawan1.Nama = "Zee";
+            karyawan1.Umur = 18;
+            karyawan1.infoperson();
+            karyawan1.infoperson("Dosen", "12345");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ inheritage/person.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritage
+{
+    public class person
+    {
+        //properti
+        public string Nama { get; set; }
+        public int Umur { get; set; }
+
+        //constructor
+        public person(string nama, int umur)
+        {
+            this.Nama = nama;
+            this.Umur = umur;
+        }
+
+        public person()
+         {
+
+         }
+
+        //fungsi
+        public void infoperson()
+        {
+            Console.WriteLine("Nama saya {0}, dan umur saya {1} tahun", this.Nama, this.Umur);
+        }
+    }
+
+    //class turunan
+    public class karyawan : person
+    {
+        //properti class turunan
+        public string karyawanID { get; set; }
+        public string subjek { get; set; }
+
+        //constructor
+        public karyawan(string karyawanID, string subjek)
+        {
+            this.subjek = subjek;
+            this.karyawanID=karyawanID;
+        }
+
+        public karyawan()
+        {
+
+        }
+
+        //override method
+        public void infoperson(string subjek, string karyawanID)
+        {
+            this.subjek = subjek;
+            this.karyawanID = karyawanID;
+            Console.WriteLine("Saya sebagai {0} dengan ID karyawan {1}" , this.subjek, this.karyawanID);
+        }
+    }
+}
